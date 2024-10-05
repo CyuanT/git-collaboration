09ed888bf3d41e39f322e6fa84faf6d0a435b2ff
RaytraceSG <azmi.maniku@gmail.com>
2024-10-05 13:01:41 +0800

    fix ec2

--- compute.tf
+++ compute.tf
@@ -1,28 +1,28 @@
-data "aws_vpc" "selected" {
-  id = var.vpc_id
-}
+# data "aws_vpc" "selected" {
+#   id = var.vpc_id
+# }
 
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = ["*public*"]
-  }
-}
+# data "aws_subnets" "public" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [var.vpc_id]
+#   }
+#   filter {
+#     name   = "tag:Name"
+#     values = ["*public*"]
+#   }
+# }
 
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = ["*private*"]
-  }
-}
+# data "aws_subnets" "private" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [var.vpc_id]
+#   }
+#   filter {
+#     name   = "tag:Name"
+#     values = ["*private*"]
+#   }
+# }
 
 data "aws_security_group" "vpc_sg_data" {
   filter {
