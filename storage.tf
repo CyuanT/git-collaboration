9fbb59c84ed88e09e5a414a53814eedbe981700f
RaytraceSG <azmi.maniku@gmail.com>
2024-10-05 11:53:54 +0800

    run terraform fmt

--- storage.tf
+++ storage.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "s3_group3" {
   bucket = "${var.bucket_region}-${var.bucket_name}"
 
   tags = {
-    Name        = "Owned by ${var.bucket_name}"
+    Name = "Owned by ${var.bucket_name}"
   }
 }
