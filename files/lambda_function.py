b2478846ee428c4323e9ea59a394841950273980
CyuanT <tan.yuan.cy@gmail.com>
2024-10-07 15:29:05 +0800

    Edited the Lamda.py response

--- files/lambda_function.py
+++ files/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    incoming_msg = "Good Afternoon from Azmi, Shafiq, and Tan Yuan"
+    incoming_msg = "Good Morning, Afternoon & Evening from  Azmi, Shafiq, and Tan Yuan"
     return {
         'statusCode': 200,
         'body': json.dumps(incoming_msg)
